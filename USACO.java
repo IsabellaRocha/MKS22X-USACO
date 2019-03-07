d42e28d59f810014cb24d94dd64ea709d12b0cc8
Isabella Rocha <irocha00@stuy.edu>
2019-03-07 14:50:50 -0500

    End of class

--- USACO.java
+++ USACO.java
@@ -8,10 +8,11 @@ public class USACO {
     int C = in.nextInt();
     int E = in.nextInt();
     int N = in.nextInt();
-    int[][] ary = new int[R][C];
-    for (int i = 0; i < R; i++) {
-      for (int idx = 0; idx < C; idx++) {
-        ary[i][idx] = in.nextInt();
+    System.out.println(R);
+    int[][] field = new int[R][C];
+    for (int idx = 0; idx < R; idx++) {
+      for (int x = 0; x < C; x++) {
+        field[idx][x] = in.nextInt();
       }
     }
     int[][] ins = new int[N][3];
@@ -20,9 +21,37 @@ public class USACO {
         ins[idx][x] = in.nextInt();
       }
     }
-    System.out.println(Arrays.toString(ary));
-    System.out.println(Arrays.toString(ins));
+
+    System.out.println(toString(field));
+    System.out.println(toString(ins));
     return 1;
+  }
+  private static String toString(int[][] field) {
+    String output = "";
+    for (int idx = 0; idx < field.length; idx++) {
+      output += "\n";
+      for (int x = 0; x < field[idx].length; x++) {
+        output += field[idx][x] + " ";
+      }
+    }
+    return output;
+  }
+  private static void stomp(int[][] field, int[][] ins, int idx) {
+    int r = ins[idx][0] - 1;
+    int c = ins[idx][1] - 1;
+    int stomps = ins[idx][2];
+    int max = Math.max(field[r][c],
+              Math.max(field[r + 1][c],
+              Math.max(field[r + 2][c],
+              Math.max(field[r][c + 1],
+              Math.max(field[r][c + 2],
+              Math.max(field[r + 1][c + 1],
+              Math.max(field[r + 1][c + 2],
+              Math.max(field[r + 2][c + 1],
+                       field[r + 2][c + 2]))))))));
+    for (int x = 0; x < stomps; x++) {
+
+    }
   }
   public static int silver(String filename) {
     return 1;
